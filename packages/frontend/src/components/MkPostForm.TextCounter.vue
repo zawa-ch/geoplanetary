1805cf496edec4e884d2a245322e595554ea1950
zawa-ch. <satellite.2e1834097@gmail.com>
2026-03-09 14:06:01 +0000

    アップストリーム変更対応
    投稿画面のメニューから見ることができる文字数制限をロール単位のものに調整

--- packages/frontend/src/components/MkPostForm.TextCounter.vue
+++ packages/frontend/src/components/MkPostForm.TextCounter.vue
@@ -23,13 +23,14 @@ import { computed, useTemplateRef } from 'vue';
 import { instance } from '@/instance.js';
 import { i18n } from '@/i18n.js';
 import number from '@/filters/number.js';
+import { $i } from '@/i';
 
 const props = defineProps<{
 	textLength: number;
 }>();
 
-const maxTextLength = computed(() => {
-	return instance ? instance.maxNoteTextLength : 1000;
+const maxTextLength = computed((): number => {
+	return $i ? $i.policies.noteLengthLimit : instance.maxNoteTextLength;
 });
 
 const textCountPercentage = computed(() => {
